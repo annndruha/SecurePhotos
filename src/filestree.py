d388091908c888e80df3ea491a57c58a1676f8f0
Andrey Marakulin <a.p.marakulin@gmail.com>
2022-07-28 00:14:41 +0300

    Almost done

--- src/filestree.py
+++ src/filestree.py
@@ -8,8 +8,8 @@ SUPPORTED_EXT = QtGui.QImageReader.supportedImageFormats()
 
 def geticon(fullpath):
     ext = os.path.splitext(os.path.basename(fullpath))[1].replace('.', '')
-    if os.path.isdir(fullpath):
-        return QtGui.QIcon('images/icons/folder.svg')
+    # if os.path.isdir(fullpath):
+    #     return QtGui.QIcon('images/icons/folder.svg')
     if ext in [str(ext, 'utf-8') for ext in SUPPORTED_EXT]:
         return QtGui.QIcon('images/icons/image.svg')
     elif ext == 'aes':
@@ -20,10 +20,11 @@ def geticon(fullpath):
 
 class FilesItem(QTreeWidgetItem):
     def __init__(self, parent, fullpath, basename):
+        super().__init__(parent, [basename])
         self.parent = parent
         self.fullpath = fullpath
         self.basename = basename
-        super().__init__(parent, [basename])
+        # self.setChildIndicatorPolicy(QTreeWidgetItem.DontShowIndicatorWhenChildless)
 
     def load_subtree(self):
         self.clear()
@@ -34,42 +35,41 @@ class FilesItem(QTreeWidgetItem):
         for element in dirlist + filelist:
 
             parent_itm = FilesItem(self, os.path.join(self.fullpath, element), os.path.basename(element))
-
-            # parent_itm = FilesItem(os.path.join(path, element))
-            # parent_itm.fullpath = os.path.join(self.fullpath, element)
             if os.path.isdir(parent_itm.fullpath):
                 parent_itm.setChildIndicatorPolicy(QTreeWidgetItem.ShowIndicator)
-            parent_itm.setIcon(0, geticon(parent_itm.fullpath))
-
+                parent_itm.setIcon(0, QtGui.QIcon('images/icons/folder.svg'))
+            else:
+                parent_itm.setIcon(0, geticon(parent_itm.fullpath))
 
     def clear(self):
-        # super().clear()
-        # self.__init__(self.parent, self.fullpath, self.basename)
-        # print('before', self.childCount())
         while self.childCount() > 0:
             for i in range(self.childCount()):
                 self.removeChild(self.child(i))
-        # for i in range(self.childCount()):
-        #     self.removeChild(self.child(i))
-        # print('after', self.childCount())
 
 
 
 
 class FilesTree(QTreeWidget):
     def __init__(self, parent):
-        super().__init__()
-        self.parent = parent
-        self.itemSelectionChanged.connect(self.preload_subtree)
+        super().__init__(parent)
+        self.itemExpanded.connect(self._item_expanded)
+        self.itemCollapsed.connect(self._item_collapsed)
+        self.setRootIsDecorated(False)
 
     def load_project_structure(self, path):
-        self.root_elem = FilesItem(self, path, os.path.basename(path))
+        self.root_elem = FilesItem(self, path, path)
         self.root_elem.load_subtree()
+        # self.root_elem.setChildIndicatorPolicy(QTreeWidgetItem.DontShowIndicator)
         self.root_elem.setExpanded(True)
+        # self.addTopLevelItem(self.root_elem)
 
-    def preload_subtree(self):
-        # super().currentItem().clear()
-        super().currentItem().load_subtree()
+    def _item_expanded(self, item):
+        item.load_subtree()
+        item.setIcon(0, QtGui.QIcon('images/icons/folder_open.svg'))
+
+    def _item_collapsed(self, item):
+        item.clear()
+        item.setIcon(0, QtGui.QIcon('images/icons/folder.svg'))
 
     def getPath(self):
         return super().currentItem().fullpath
