511794b83a174f93d7696212e3651b153e27875a
Андрей Маракулин <annndruha.github@gmail.com>
2025-03-14 18:52:58 +0300

    Update settings.py

--- src/utils/settings.py
+++ src/utils/settings.py
@@ -1,40 +1,80 @@
 import json
-import os.path
+import os
 
-# TODO: NOT DONE
-class KeyValueDataBase(dict):
+
+class DBJsonFile:
     def __init__(self):
-        super().__init__()
-        self.file_path = os.path.join(os.path.expanduser("~"), '.SecurePhotos', 'config.json')
-        self.data: dict = {}
+        self.filename = os.path.join(os.path.expanduser("~"), '.SecurePhotos', 'config.json')
+        self._version = 'v1'
+        self._load()
 
-        if os.path.exists(self.file_path):
-            with open(self.file_path, 'r', encoding='utf-8') as f:
-                self.data = json.load(f)
+    def _load(self):
+        if os.path.exists(self.filename):
+            with open(self.filename, 'r', encoding='utf-8') as f:
+                try:
+                    self.store = json.load(f)[self._version]
+                except KeyError:
+                    self.store = {}
+                except json.JSONDecodeError:
+                    self.store = {}
         else:
-            self._dump_to_disk()
+            self.store = {self._version: {}}
 
-    def is_key_exists(self, key) -> bool:
-        return str(key) in self.data.keys()
-
-    def __getitem__(self, key) -> str | None:
-        if str(key) not in self.data.keys():
-            return None
-        return self.data[str(key)]
-
-    def __setitem__(self, _id, token) -> None:
-        assert isinstance(_id, int | str)
-        assert isinstance(token, str)
-        self.data[str(_id)] = token
-        self._dump_to_disk()
-
-    def __delitem__ (self, _id) -> None:
-        if str(_id) in self.data.keys():
-            del self.data[str(_id)]
-            self._dump_to_disk()
-
-    def _dump_to_disk(self):
-        dirs, filename = os.path.split(self.file_path)
+    def _save(self):
+        dirs, filename = os.path.split(self.filename)
         os.makedirs(dirs, exist_ok=True)
-        with open(self.file_path, 'w', encoding='utf-8') as f:
-            json.dump(self.data, f, indent=4, ensure_ascii=False)
+        with open(self.filename, "w", encoding="utf-8") as f:
+            json.dump({self._version: self.store}, f, indent=4, ensure_ascii=False)
+
+    def __getitem__(self, key: str):
+        key = str(key)
+        if key not in self.store:
+            return None
+        return self.store[key]
+
+    def __setitem__(self, key: str, value):
+        key = str(key)
+        self.store[key] = value
+        self._save()
+
+    def __delitem__(self, key: str):
+        key = str(key)
+        if key in self.store:
+            del self.store[key]
+            self._save()
+
+    def __contains__(self, key: str):
+        key = str(key)
+        return key in self.store
+
+    def keys(self):
+        return self.store.keys()
+
+    def values(self):
+        return self.store.values()
+
+    def items(self):
+        return self.store.items()
+
+    def get(self, key: str, default=None):
+        key = str(key)
+        if key not in self.store:
+            return None
+        return self.store.get(key, default)
+
+    def pop(self, key: str, default=None):
+        key = str(key)
+        value = self.store.pop(key, default)
+        self._save()
+        return value
+
+    def clear(self):
+        self.store.clear()
+        self._save()
+
+    def __repr__(self):
+        return repr(self.store)
+
+
+db = DBJsonFile()
+print('')
